78f591be12156fa343832afc6c2730d75a6b276c
vijsak27 <146001383+vijsak27@users.noreply.github.com>
2026-02-25 00:40:55 +0000

    adding comments to Session.java

--- Session.java
+++ Session.java
@@ -1,14 +1,25 @@
+/*
+The Session.java class sets up the structure of a session object
+The Session object will contain 3 pieces of info (as of now)
+1. sessionID
+2. sessionName
+3. presenterName
+*/
+
 public class Session{
+    //variables contained in session
     private int sessionID;
     private String sessionName;
     private String presenterName;
     
+    //constructor
     public Session(int ID, String name, String presenter){
         sessionID = ID;
         sessionName = name;
         presenterName = presenter;
     }
-
+    
+    //returns the ID of the session object getID() is called on
     public int getID(){
         return sessionID;
     }
