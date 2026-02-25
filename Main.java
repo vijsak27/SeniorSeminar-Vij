4f48300402843d843b235a39701aef102f443ac5
vijsak27 <146001383+vijsak27@users.noreply.github.com>
2026-02-25 00:44:11 +0000

    adding comments to Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 import java.util.*;
 
+/*
+The Main class will load in the files and run the program to 
+organize the students and sessions for senior seminar
+*/
+
 public class Main{
 	public static void main (String[] args){
-		ReadFile rf1 = new ReadFile();
-		rf1.loadStudents();
-		rf1.loadSessions();
-		Schedule s1 = new Schedule();
-		s1.scheduleDetails();
-		//s1.rankedPopularityAllSlots();
-		//I am currently working on this aspect (ranking the popularity)
+		ReadFile rf1 = new ReadFile();//create ReadFile object to load in information from CSV files
+		rf1.loadStudents();//load the students
+		rf1.loadSessions();//load the sessions
+		Schedule s1 = new Schedule();//create a schedule object
+		s1.scheduleDetails();//prompt user for details about the event they are planning for (number of time slots, sessions, etc.)
+		s1.rankedPopularityAllSlots();//I am currently working on this aspect (ranking the popularity) to fix logic errors
+		
 	
 	}
 
